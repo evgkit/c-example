d555fb9b94bd10a03fb7ce30b4bc18c33a548dbc
evgkit <kitaev.ea@gmail.com>
2014-01-12 21:27:58 +0400

    upgrade "hello_world" code
    ..super-code approach!

--- ch1/hello_world.c
+++ ch1/hello_world.c
@@ -2,5 +2,12 @@
 
 int main(void) {
     printf("Hello, world!\n");
+    printf("EOF - %d\n", EOF);
+
+    int c = 0;
+    while((c = getchar()) != EOF) {
+        putchar(c);
+    }
+
     return 0;
 }
