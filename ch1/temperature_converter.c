434ae8e7367510c5f00cd4dffd53130b36374455
evgkit <kitaev.ea@gmail.com>
2014-01-12 20:39:58 +0400

    36.6
    ..

--- ch1/temperature_converter.c
+++ ch1/temperature_converter.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#define CTEMP 36.6
 
 int main(void) {
-    int t = 0;
-    scanf(t);
-    printf("%3d -> %6.1f \n", t, (9.0/5.0) * (t + 32.0));
+    printf("Celsius to Fahrenheit:\n");
+    printf("%3.1f -> %3.1f \n", CTEMP, (9.0/5.0) * CTEMP + 32.0);
     return 0;
 }
 
